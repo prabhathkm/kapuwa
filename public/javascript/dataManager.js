7d35f36523d8738b18509b807a0808b5441acb67
Prabhath De Silva <prabhaths@leapset.com>
2016-03-29 14:46:18 -0500

    code cleanup

--- public/javascript/dataManager.js
+++ public/javascript/dataManager.js
@@ -39,64 +39,6 @@ var DataManager = {
 
 };
 
-//
-//var ajaxReq = null;
-//var prevReq = null;
-//var ajaxCall = function(ajaxParam, successFunction, errorFunction, completeFunction) {
-//
-//  var ajaxBody = {};
-//
-//  _.each(ajaxParam, function(value, key){
-//    ajaxBody[key] = value;
-//  });
-//
-//  ajaxBody['beforeSend'] = function(jqXHR, jqReq){
-//    //abort previous request if its for the same target
-//    if(prevReq && prevReq.url === jqReq.url && prevReq.type === jqReq.type){
-//      if(ajaxReq != null){
-//        ajaxReq.abort();
-//      }
-//    }
-//
-//    prevReq = jqReq
-//  };
-//
-//  ajaxBody['success'] = successFunction;
-//
-//  ajaxBody['error'] = function(jqXHR, status, errorThrown) {
-//
-//    if(jqXHR.status == "401") {
-//      window.location.href = "/";
-//    }
-//
-//    if(typeof errorFunction == "function") {
-//      errorFunction(jqXHR, status, errorThrown);
-//    }
-//  };
-//
-//  ajaxBody['complete'] = function(jqXHR, status) {
-//    if(typeof completeFunction == "function") {
-//      completeFunction(jqXHR, status);
-//    }
-//  };
-//
-//  ajaxReq = jQuery.ajax(ajaxBody);
-//};
-//
-//
-//var ajaxPostModel = function (url,data,cb) {
-//  ajaxCall({
-//      url: url,
-//      type: "POST",
-//      dataType: 'json',
-//      data: data,
-//      timeout: 60000
-//    },
-//    function(resp) {
-//      cb(resp);
-//    });
-//};
-
 
 var ajaxCallStack = {};
 var ajaxPostModel = function (idFields, url,data,cb) {
